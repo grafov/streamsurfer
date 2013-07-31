673d69a11f3374f31d0774c13c5f769ba49cdc6a
Alexander I.Grafov <grafov@gmail.com>
2013-08-01 01:14:29 +0400

    Still don't work.

--- source-loader.go
+++ source-loader.go
@@ -13,15 +13,16 @@ func SourceLoader(confile string) {
 	ReadConfig(confile)
 }
 
-func ReadConfig(confile string) (cfg config) {
+func ReadConfig(confile string) (cfg Config) {
 	/*	cfg = config{
-			streams:  stream{name: "localhost:1234"},
+			Streams:  stream{name: "localhost:1234"},
 			workers:  workers{1, 1},
 			timeouts: timeouts{12, 12},
-			options:  options{tryOneSegment: true},
+			Options:  Options{tryOneSegment: true},
 		}
 	*/
 	//cfg = make(map[string]interface{})
+	cfg = Config{}
 	// TODO второй конфиг из /etc/hlsproberc
 	if confile == "" {
 		confile = "~/.hlsproberc"
@@ -29,51 +30,19 @@ func ReadConfig(confile string) (cfg config) {
 	data, e := ioutil.ReadFile(os.ExpandEnv(strings.Replace(confile, "~", os.Getenv("HOME"), 1)))
 	if e == nil {
 		e = goyaml.Unmarshal(data, &cfg)
-		fmt.Printf("%+v", cfg)
+		fmt.Printf("%+v %+v", cfg, e)
 	} else {
 		print("Config file not found or parsing failed. Hardcoded defaults used.\n")
 	}
-	/*	for k, v := range cfg {
-			switch vv := v.(type) {
-			case string:
-				fmt.Println(k, "is string", vv)
-			case int:
-				fmt.Println(k, "is int", vv)
-			case map[string]interface{}:
-				fmt.Println(k, "is an config:")
-				for i, u := range vv {
-					fmt.Println(i, u)
-				}
-			case map[string]interface{}:
-				fmt.Println(k, "is an dict:")
-				for i, u := range vv {
-					fmt.Println(i, u)
-				}
-			case []interface{}:
-				fmt.Println(k, "is an array:")
-				for i, u := range vv {
-					fmt.Println(i, u)
-				}
-			case map[interface{}]interface{}:
-				fmt.Println(k, "is an map:")
-				for i, u := range vv {
-					fmt.Println(i, u)
-				}
-			default:
-				fmt.Printf("%s is of a type %s I don't know how to handle\n", k, v)
-			}
-		}
-	*/
 	return
 }
 
 /* Datatypes */
 
-type config struct {
-	streams  []string
-	workers  workers `yaml:"workers"`
-	timeouts timeouts
-	options  options
+type Config struct {
+	Streams []string `yaml:"streams"`
+	Workers Workers  `yaml:"workers"`
+	Options Options  `yaml:"options"`
 }
 
 //type stream map[string]interface{}
@@ -83,16 +52,11 @@ type stream struct {
 	//members *stream // link to stream group members (nil if the item is a stream not a group)
 }
 
-type workers struct {
-	streamProbers uint `yaml:"stream-probers,omitempty"`
-	mediaProbers  uint `yaml:"media-probers"`
+type Workers struct {
+	StreamProbers uint `yaml:"stream-probers,omitempty"`
+	MediaProbers  uint `yaml:"media-probers"`
 }
 
-type timeouts struct {
-	playlistRead int
-	mediaRead    int
-}
-
-type options struct {
-	tryOneSegment bool
+type Options struct {
+	TryOneSegment bool `yaml:"one-segment"`
 }
