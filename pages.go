8acc2fc633b76025daff7d8370291743027e294f
Alexander I.Grafov <grafov@gmail.com>
2013-10-24 01:31:40 +0400

    Forgot pages file.

---
+++ pages.go
@@ -0,0 +1,13 @@
+// Web reports generator
+package main
+
+import (
+	//"github.com/hoisie/mustache" // TODO migrate back to native golang templates
+	"html/template"
+)
+
+func MainPage() []byte {
+	// page := mustache.Render(MainPageTemplate, StatGlobals)
+	// return []byte(page)
+
+}
